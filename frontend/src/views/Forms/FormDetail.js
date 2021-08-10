66b0c129a14ecee93b2fc397d4988f7e8941f283
Puzaan Xhres <41267289+puzaan@users.noreply.github.com>
2021-08-10 17:01:57 +0545

    admin and frontend in same project

--- frontend/src/views/Forms/FormDetail.js
+++ frontend/src/views/Forms/FormDetail.js
@@ -1,143 +1,140 @@
-// import React, { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { Typography, } from "@material-ui/core";
-// import { DataGrid } from '@material-ui/data-grid';
-// import { formLists } from "../../action/FormDetailAction";
-// import DeleteIcon from "@material-ui/icons/Delete";
-// import { Link } from "react-router-dom";
-// import Loder from 'views/Loder';
-// import Alerts from 'views/Alert/Alert';
-// import { makeStyles } from "@material-ui/styles";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Typography, } from "@material-ui/core";
+import { DataGrid } from '@material-ui/data-grid';
+import { formLists } from "../../action/FormDetailAction";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { Link } from "react-router-dom";
+import Loder from 'views/Loder';
+import Alerts from 'views/Alert/Alert';
+import { makeStyles } from "@material-ui/styles";
 
+const useStyle = makeStyles(() => ({
+    userListEdit: {
+        border: "none",
+        borderRadius: "5px 10px",
+        cursor: "pointer",
+        marginRight: '20px',
+        backgroundColor: '#3bb077',
+        color: "white",
+    },
+}))
 
+const FormDetail = ({ history }) => {
 
+    const classes = useStyle();
+    const columns = [
+        // { field: "_id", headerName: "ID", width: 90, hide: true},
+        {
+            field: "fullName",
+            headerName: "Name",
+            width: 140,
+        },
+        {
+            field: "email",
+            headerName: "Email",
+            sortable: false,
+            width: 160,
+        },
+        {
+            field: "phone",
+            headerName: "Contact No",
+            sortable: false,
+            width: 120,
+        },
+        {
+            field: "education",
+            headerName: "Education",
+            width: 140,
+            sortable: false,
+        },
+        {
+            field: "createdAt",
+            headerName: "Date",
+            width: 170,
+        },
+        {
+            field: "course",
+            headerName: "Course To Enroll",
+            sortable: false,
+            width: 160,
+        },
 
-// const useStyle = makeStyles(() => ({
-//     userListEdit: {
-//         border: "none",
-//         borderRadius: "5px 10px",
-//         cursor: "pointer",
-//         marginRight: '20px',
-//         backgroundColor: '#3bb077',
-//         color: "white",
-//     },
-// }))
+        {
+            field: "college",
+            headerName: "Collage Name",
+            sortable: false,
+            width: 160,
+        },
 
-// const FormDetail = ({ history }) => {
-
-//     const classes = useStyle();
-//     const columns = [
-//         // { field: "_id", headerName: "ID", width: 90, hide: true},
-//         {
-//             field: "fullName",
-//             headerName: "Name",
-//             width: 140,
-//         },
-//         {
-//             field: "email",
-//             headerName: "Email",
-//             sortable: false,
-//             width: 160,
-//         },
-//         {
-//             field: "phone",
-//             headerName: "Contact No",
-//             sortable: false,
-//             width: 120,
-//         },
-//         {
-//             field: "education",
-//             headerName: "Education",
-//             width: 140,
-//             sortable: false,
-//         },
-//         {
-//             field: "createdAt",
-//             headerName: "Date",
-//             width: 170,
-//         },
-//         {
-//             field: "course",
-//             headerName: "Course To Enroll",
-//             sortable: false,
-//             width: 160,
-//         },
-
-//         {
-//             field: "college",
-//             headerName: "Collage Name",
-//             sortable: false,
-//             width: 160,
-//         },
-
-//         {
-//             field: "message",
-//             headerName: "Messages",
-//             sortable: false,
-//             width: 160,
-//         },
-//         {
-//             field: "action",
-//             headerName: "Action",
-//             width: 140,
-//             renderCell: (params) => {
-//                 return (
+        {
+            field: "message",
+            headerName: "Messages",
+            sortable: false,
+            width: 160,
+        },
+        {
+            field: "action",
+            headerName: "Action",
+            width: 140,
+            renderCell: (params) => {
+                return (
                    
-//                         <Link to={`/form/${params.row._id}`}>
-//                             <button className={classes.userListEdit}>View</button>
-//                         </Link>
+                        <Link to={`/form/${params.row._id}`}>
+                            <button className={classes.userListEdit}>View</button>
+                        </Link>
                    
-//                 );
-//             },
-//         },
-//     ];
-//     const dispatch = useDispatch();
-//     const userLogin = useSelector((state) => state.userLogin);
-//     const { userInfo } = userLogin;
+                );
+            },
+        },
+    ];
+    const dispatch = useDispatch();
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo } = userLogin;
 
-//     const formList = useSelector((state) => state.formList);
-//     const { loding, error, lists } = formList;
+    const formList = useSelector((state) => state.formList);
+    const { loding, error, lists } = formList;
     
     
-//     useEffect(() => {
+    useEffect(() => {
 
-//         if (!userInfo) {
-//             history.push("/");
-//         }
-//         dispatch(formLists());
-//     }, [dispatch]);
+        if (!userInfo) {
+            history.push("/");
+        }
+        dispatch(formLists());
+    }, [dispatch]);
 
-//     return (
-//         <div>
-        
-
-//             <div style={{ height: 400, width: "95%", padding: "20px" }}>
-//                 <Typography variant="h4"> All User Forms</Typography>
-//                 {error && <Alerts severity="error"> {error}</Alerts>}
-//                 {loding && <Loder />}
-//                 <DataGrid
-//                     rows={lists}
-//                     columns={columns}
-//                     pageSize={5}
-//                     checkboxSelection
-//                     disableSelectionOnClick
-//                 />
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default FormDetail;
-
-
-import React from 'react'
-
-const FormDetail = () => {
     return (
         <div>
-            dsadsf
-        </div>
-    )
-}
+        
 
-export default FormDetail
+            <div style={{ height: 400, width: "95%", padding: "20px" }}>
+                <Typography variant="h4"> All User Forms</Typography>
+                {error && <Alerts severity="error"> {error}</Alerts>}
+                {loding && <Loder />}
+                <DataGrid
+                    rows={lists}
+                    columns={columns}
+                    pageSize={5}
+                    checkboxSelection
+                    disableSelectionOnClick
+                />
+            </div>
+        </div>
+    );
+};
+
+export default FormDetail;
+
+
+// import React from 'react'
+
+// const FormDetail = () => {
+//     return (
+//         <div>
+//             dsadsf
+//         </div>
+//     )
+// }
+
+// export default FormDetail
